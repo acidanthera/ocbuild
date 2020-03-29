093559d8ed707734be96b38eb16fcc36ec832093
vit9696 <vit9696@users.noreply.github.com>
2020-03-29 11:58:02 +0300

    Fix unzipping mtoc

--- efibuild.sh
+++ efibuild.sh
@@ -81,7 +81,7 @@ if [ "$(which mtoc)" == "" ]; then
   mtoczip=$(cat mtoc-mac64.zip)
   rm -rf mtoc-*
   curl -OL "https://github.com/acidanthera/ocbuild/raw/master/external/${mtoczip}" || exit 1
-  unzip -q mtoc-mac64.zip mtoc || exit 1
+  unzip -q "${mtoczip}" mtoc || exit 1
   sudo mkdir -p /usr/local/bin || exit 1
   sudo cp mtoc /usr/local/bin/mtoc || exit 1
   popd >/dev/null
