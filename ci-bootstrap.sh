92ae64dc44ad00e3c7dc2036bd467713dc1db110
Dhinak G <dhinak2004@gmail.com>
2020-11-26 17:20:32 -0500

    Add multiple OS support to ci-bootstrap.sh

--- ci-bootstrap.sh
+++ ci-bootstrap.sh
@@ -1,54 +1,84 @@
 #!/bin/bash
 
-XCODE_DIR="/Applications/Xcode_VERSION.app/Contents/Developer"
+unamer() {
+  NAME="$(uname)"
 
-# In GitHub Actions:
-# env:
-#  PROJECT_TYPE: "UEFI"
+  if [ "$(echo "${NAME}" | grep MINGW)" != "" ] || [ "$(echo "${NAME}" | grep MSYS)" != "" ]; then
+    echo "Windows"
+  else
+    echo "${NAME}"
+  fi
+}
 
-case "${PROJECT_TYPE}" in 
-    UEFI)
-        BUILD_DEVELOPER_DIR="${XCODE_DIR/VERSION/12.2}"
-        ANALYZE_DEVELOPER_DIR="${XCODE_DIR/VERSION/12.2}"
-        COVERITY_DEVELOPER_DIR="${XCODE_DIR/VERSION/12.2}"
-        ;;
-    
-    KEXT | TOOL)
-        BUILD_DEVELOPER_DIR="${XCODE_DIR/VERSION/12.2}"
-        ANALYZE_DEVELOPER_DIR="${XCODE_DIR/VERSION/12.2}"
-        COVERITY_DEVELOPER_DIR="${XCODE_DIR/VERSION/12.2}"
-        ;;
-    
-    *)
-        echo "ERROR: Invalid project type!"
+if [ "$(unamer)" = "Darwin" ]; then
+    XCODE_DIR="/Applications/Xcode_VERSION.app/Contents/Developer"
+
+    # In GitHub Actions:
+    # env:
+    #  PROJECT_TYPE: "UEFI"
+
+    case "${PROJECT_TYPE}" in 
+        UEFI)
+            BUILD_DEVELOPER_DIR="${XCODE_DIR/VERSION/12.2}"
+            ANALYZE_DEVELOPER_DIR="${XCODE_DIR/VERSION/12.2}"
+            COVERITY_DEVELOPER_DIR="${XCODE_DIR/VERSION/12.2}"
+            ;;
+        
+        KEXT | TOOL)
+            BUILD_DEVELOPER_DIR="${XCODE_DIR/VERSION/12.2}"
+            ANALYZE_DEVELOPER_DIR="${XCODE_DIR/VERSION/12.2}"
+            COVERITY_DEVELOPER_DIR="${XCODE_DIR/VERSION/12.2}"
+            ;;
+        
+        *)
+            echo "ERROR: Invalid project type!"
+            exit 1
+            ;;
+    esac
+
+    SELECTED_DEVELOPER_DIR="${JOB_TYPE}_DEVELOPER_DIR"
+
+    if [ -z "${!SELECTED_DEVELOPER_DIR}" ]; then
+        echo "ERROR: Invalid or missing job type!"
         exit 1
-        ;;
-esac
+    fi
 
-SELECTED_DEVELOPER_DIR="${JOB_TYPE}_DEVELOPER_DIR"
+    echo "DEVELOPER_DIR=${!SELECTED_DEVELOPER_DIR}" >> $GITHUB_ENV
 
-if [ -z "${!SELECTED_DEVELOPER_DIR}" ]; then
-    echo "ERROR: Invalid or missing job type!"
-    exit 1
+    # Since GITHUB_ENV doesn't affect the current step, need to export DEVELOPER_DIR for subsequent commands.
+    export DEVELOPER_DIR="${!SELECTED_DEVELOPER_DIR}"
 fi
 
-echo "DEVELOPER_DIR=${!SELECTED_DEVELOPER_DIR}" >> $GITHUB_ENV
-
-# Since GITHUB_ENV doesn't affect the current step, need to export DEVELOPER_DIR for subsequent commands.
-export DEVELOPER_DIR="${!SELECTED_DEVELOPER_DIR}"
+colored_text() {
+    echo -e "\033[0;36m${1}\033[0m"
+}
 
 # Print runner details
-echo -e '\033[0;36mmacOS version\033[0m'
-sw_vers
+colored_text "OS version"
+if [ "$(unamer)" = "Darwin" ]; then
+    sw_vers
+elif [ "$(unamer)" = "Windows" ]; then
+    wmic os get caption, version
+else
+    lsb_release -a
+fi
 
-echo -e '\033[0;36mgit version\033[0m'
+colored_text "git version"
 git --version
 
-echo -e '\033[0;36mbash version\033[0m'
+colored_text "bash version"
 bash --version
 
-echo -e '\033[0;36mclang version\033[0m'
-clang --version
+colored_text "curl version"
+curl --version
 
-echo -e '\033[0;36mXcode version\033[0m'
-xcode-select --print-path
+if [ "$(unamer)" = "Darwin" ]; then
+    colored_text "clang version"
+    clang --version
+
+    colored_text "Xcode version"
+    xcode-select --print-path
+else
+    colored_text "gcc version"
+    gcc --version
+fi
