f885d8b39ff24d643875a333446ace89dc98d7a6
Vitaly Cheptsov <4348897+vit9696@users.noreply.github.com>
2022-08-14 01:05:03 +0300

    Allow adding extra codesign flags

--- codesign/appsign.sh
+++ codesign/appsign.sh
@@ -167,6 +167,8 @@ if [ "$identifier" = "" ]; then
   abort "Unable to locate application identifier" "Set CFBundleIdentifier in Info.plist"
 fi
 
+extraflags="$3"
+
 # Obtain certificate data.
 echo "Downloading the certificates..."
 downloadcert
@@ -174,15 +176,15 @@ downloadcert
 # Codesign inner applications.
 echo "Codesigning inner applications..."
 find "${apppath}"/* -name '*.app' -exec \
-  /usr/bin/codesign --force --deep --options runtime -s "Developer ID" {} \; || abort "Unable to sign inner applications"
+  /usr/bin/codesign --force --deep --options runtime $extraflags -s "Developer ID" {} \; || abort "Unable to sign inner applications"
 
 echo "Codesigning inner plugins..."
 find "${apppath}"/* -name '*.qlgenerator' -exec \
-  /usr/bin/codesign --force --deep --options runtime -s "Developer ID" {} \; || abort "Unable to sign inner plugins"
+  /usr/bin/codesign --force --deep --options runtime $extraflags -s "Developer ID" {} \; || abort "Unable to sign inner plugins"
 
 # Codesign the application.
 echo "Codesigning application..."
-/usr/bin/codesign --force --deep --options runtime -s "Developer ID" "${apppath}" || abort "Unable to sign application"
+/usr/bin/codesign --force --deep --options runtime $extraflags -s "Developer ID" "${apppath}" || abort "Unable to sign application"
 
 # Compress the application.
 echo "Compressing application..."
