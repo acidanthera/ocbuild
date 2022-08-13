7659a8fb8575a02b27960f8e73c78cd0c52b36f4
Vitaly Cheptsov <4348897+vit9696@users.noreply.github.com>
2022-08-14 01:20:07 +0300

    Fix previous commit x2

--- codesign/appsign.sh
+++ codesign/appsign.sh
@@ -167,7 +167,8 @@ if [ "$identifier" = "" ]; then
   abort "Unable to locate application identifier" "Set CFBundleIdentifier in Info.plist"
 fi
 
-extraflags=($(echo "$3")) #shellcheck disable=SC2207
+#shellcheck disable=SC2207
+extraflags=($(echo "$3"))
 
 # Obtain certificate data.
 echo "Downloading the certificates..."
