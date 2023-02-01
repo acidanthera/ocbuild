d25843498a8369c216f5ae2b1308c515beeecef8
vit9696 <vit9696@users.noreply.github.com>
2023-02-01 09:44:59 +0300

    Revert "efibuild: Adds support for plain string in build arguments (#20)"
    This reverts commit 601163f307f19a51afb16c7b6a33fdf4712f1f5f.

--- efibuild.sh
+++ efibuild.sh
@@ -13,16 +13,6 @@ if [ "$OFFLINE_MODE" = "" ]; then
   OFFLINE_MODE=0
 fi
 
-is_array()
-{
-    # Detects if argument is an array, returns 1 on success, 0 otherwise
-    [ -z "$1" ] && return 0
-    if [ -n "$BASH" ]; then
-      declare -p "${1}" 2> /dev/null | grep 'declare \-a' >/dev/null && return 1
-    fi
-    return 0
-}
-
 prompt() {
   echo "$1"
   if [ "$FORCE_INSTALL" != "1" ]; then
@@ -261,31 +251,11 @@ if [ "$RELPKG" = "" ]; then
   RELPKG="$SELFPKG"
 fi
 
-if [[ ! $(is_array ARCHS) ]]; then
-  IFS=', ' read -r -a ARCHS <<< "$ARCHS"
-fi
-
-if [[ ! $(is_array ARCHS_EXT) ]]; then
-  IFS=', ' read -r -a ARCHS_EXT <<< "$ARCHS_EXT"
-fi
-
-if [[ ! $(is_array TOOLCHAINS) ]]; then
-  IFS=', ' read -r -a TOOLCHAINS <<< "$TOOLCHAINS"
-fi
-
-if [[ ! $(is_array TARGETS) ]]; then
-  IFS=', ' read -r -a TARGETS <<< "$TARGETS"
-fi
-
-if [[ ! $(is_array RTARGETS) ]]; then
-  IFS=', ' read -r -a RTARGETS <<< "$RTARGETS"
-fi
-
-if [ "${ARCHS[*]}" = "" ]; then
+if [ "$ARCHS" = "" ]; then
   ARCHS=('X64')
 fi
 
-if [ "${TOOLCHAINS[*]}" = "" ]; then
+if [ "$TOOLCHAINS" = "" ]; then
   if [ "$(unamer)" = "Darwin" ]; then
     TOOLCHAINS=('XCODE5')
   elif [ "$(unamer)" = "Windows" ]; then
@@ -295,7 +265,7 @@ if [ "${TOOLCHAINS[*]}" = "" ]; then
   fi
 fi
 
-if [ "${TARGETS[*]}" = "" ]; then
+if [ "$TARGETS" = "" ]; then
   TARGETS=('DEBUG' 'RELEASE' 'NOOPT')
 elif [ "${RTARGETS[*]}" = "" ]; then
   RTARGETS=("${TARGETS[@]}")
