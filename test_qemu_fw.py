5101a6f6054fef06bc3ceed3ad1b99d0b58b61ad
Savva Mitrofanov <savvamtr@gmail.com>
2023-03-23 13:51:08 +0600

    test_qemu_fw.py: Add support testing ARM/ARM64 Linux and Windows PE images

--- test_qemu_fw.py
+++ test_qemu_fw.py
@@ -11,6 +11,7 @@ import logging
 import pexpect
 
 TEST_LINUX_PATH = "./TestLinux"
+TEST_WINPE_PATH = "./winpe.iso"
 TESTCONSOLE_PATH = "./TestConsole"
 
 
@@ -32,7 +33,7 @@ def get_qemu_version() -> tuple:
     return version
 
 
-def test_firmware(fw_path: str, boot_drive_path: str, expected_string: str, timeout: int, rdrand: bool) -> bool:
+def test_firmware(fw_path: str, boot_drive_path: str, expected_string: str, timeout: int, rdrand: bool, fw_arch: str) -> bool:
     """
     Run QEMU and check whether firmware can start given image
 
@@ -45,19 +46,34 @@ def test_firmware(fw_path: str, boot_drive_path: str, expected_string: str, time
     """
     result = False
     qemu_version = get_qemu_version()
-    if rdrand:
-        if qemu_version is not None and qemu_version < (6, 2, 0):
-            p = pexpect.spawn('qemu-system-x86_64 -enable-kvm -display none -bios ' + fw_path +
-                              ' -machine q35 -m 2048 -cpu Penryn,+rdrand -smp 2 -usb -device usb-mouse '
-                              '-serial stdio ' + boot_drive_path)
-        else:
-            p = pexpect.spawn('qemu-system-x86_64 -display none -bios ' + fw_path +
-                              ' -machine q35 -m 2048 -cpu Penryn,+rdrand -smp 2 -usb -device usb-mouse '
-                              '-serial stdio ' + boot_drive_path)
+    if qemu_version is None:
+        logging.error("Can't retrieve QEMU version!")
+        return False
+
+    qemu_x86_runner = f"qemu-system-x86_64 {'-enable-kvm ' if qemu_version < (6, 2, 0) else ''}"
+    qemu_arm_runner = 'qemu-system-arm '
+    qemu_arm64_runner = 'qemu-system-aarch64 '
+    machine_string_x86 = f" -cpu Penryn{',+rdrand' if rdrand else ''} -smp 2 -machine q35 -m 2048 "
+    machine_string_arm = ' -cpu cortex-a15 -smp 2 -machine virt,highmem=off ' \
+                         ' -accel tcg,tb-size=1024 -m 2048 '
+    machine_string_arm64 = ' -cpu cortex-a53 -smp 2 -machine virt,virtualization=on ' \
+                           '-accel tcg,tb-size=1024 -m 2048 '
+    if fw_arch == "x86":
+        p = pexpect.spawn(qemu_x86_runner + machine_string_x86 +
+                          '-bios ' + fw_path + ' -display none -serial stdio '
+                          + boot_drive_path)
+    elif fw_arch == "arm":
+        p = pexpect.spawn(qemu_arm_runner + machine_string_arm +
+                          '-bios ' + fw_path + ' -display none -serial stdio '
+                          + boot_drive_path)
+    elif fw_arch == "arm64":
+        p = pexpect.spawn(qemu_arm64_runner + machine_string_arm64 +
+                          '-bios ' + fw_path + ' -display none -serial stdio '
+                          + boot_drive_path)
     else:
-        p = pexpect.spawn('qemu-system-x86_64 -display none -bios ' + fw_path +
-                          ' -machine q35 -m 2048 -cpu Penryn -smp 2 -usb -device usb-mouse '
-                          '-serial stdio ' + boot_drive_path)
+        logging.error("Unsupported arch specified!")
+        return False
+
     p.timeout = timeout
     res = p.expect([expected_string, pexpect.TIMEOUT])
     if res == 0:
@@ -112,6 +128,16 @@ def prepare_test_linux_image(esp_path: str) -> bool:
     return True
 
 
+def parse_fw_arch(fw_arch: str) -> str:
+    if fw_arch in ['i386', 'x86', 'IA32', 'X64', 'x64', 'x86_64', 'X86']:
+        return 'x86'
+    if fw_arch in ['arm', 'arm32', 'aarch32', 'AARCH32', 'ARM']:
+        return 'arm'
+    if fw_arch in ['AARCH64', 'aarch64', 'arm64', 'ARM64', 'AA64', 'aa64']:
+        return 'arm64'
+    return "Unknown"
+
+
 def main():
     """ The QEMU-based firmware checker """
     parser = argparse.ArgumentParser(description='Run QEMU and determine whether firmware can start bootloader.')
@@ -120,16 +146,23 @@ def main():
     parser.add_argument('--test-console-path', dest='user_testconsole_path', action='store')
     parser.add_argument('--test-linux-path', dest='user_testlinux_path', action='store')
     parser.add_argument('--test-linux', dest='test_linux', action='store_true')
+    parser.add_argument('--test-winpe-path', dest='user_testwinpe_path', action='store')
+    parser.add_argument('--test-winpe', dest='test_winpe', action='store_true')
+    parser.add_argument('--fw-arch', dest='fw_arch', action='store')
     parser.set_defaults(rdrand=True)
     parser.set_defaults(test_linux=False)
+    parser.set_defaults(fw_arch="x86")
     pexpect_timeout = 30  # default 30
     testconsole_path = TESTCONSOLE_PATH
     testlinux_path = TEST_LINUX_PATH
+    testwinpe_path = TEST_WINPE_PATH
 
     args = parser.parse_args()
     logging.basicConfig(
         format="%(asctime)-15s [%(levelname)s] %(funcName)s: %(message)s",
         level=logging.INFO)
+
+    fw_arch = parse_fw_arch(args.fw_arch)
 
     if not args.test_linux and args.user_testlinux_path:
         parser.error("--test-linux-path requires --test-linux")
@@ -138,6 +171,8 @@ def main():
         testlinux_path = args.user_testlinux_path
     elif args.user_testconsole_path is not None:
         testconsole_path = args.user_testconsole_path
+    elif args.user_testwinpe_path is not None:
+        testwinpe_path = args.user_testwinpe_path
 
     if args.test_linux:
         if not prepare_test_linux_image(testlinux_path):
@@ -145,13 +180,17 @@ def main():
         boot_drive = '-hda fat:rw:' + testlinux_path
         expected_string = 'Hello World!'
         pexpect_timeout = 120
+    elif args.test_winpe:
+        boot_drive = '-cdrom ' + testwinpe_path
+        expected_string = 'EVENT: The CMD command is now available'
+        pexpect_timeout = 120
     else:
         if not prepare_test_console(testconsole_path):
             sys.exit(1)
         boot_drive = '-hda fat:rw:' + testconsole_path
-        expected_string = 'Partition #3 GPT entry is not accessible'
+        expected_string = 'GPT entry is not accessible'
     print("Testing ...")
-    if test_firmware(args.fw_path, boot_drive, expected_string, pexpect_timeout, args.rdrand):
+    if test_firmware(args.fw_path, boot_drive, expected_string, pexpect_timeout, args.rdrand, fw_arch):
         sys.exit(0)
     else:
         sys.exit(1)
