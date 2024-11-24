1b6d6049c95eeeb8a37a7246abf4cd1782d3e208
Mike Beaton <mjsbeaton@gmail.com>
2024-11-24 16:43:12 +0000

    ci-bootstrap: Stick to 15.2 because current default download of Coverity cannot support higher
     - https://sig-synopsys.my.site.com/community/s/article/coverity-capture-failed-for-Xcode-15-4-version-on-Mac-OS
     - https://documentation.blackduck.com/bundle/coverity-docs/page/webhelp-files/relnotes_latest.html#:~:text=Bug%20fixes-,CAP%2D2296,-Reported%20in%20version

--- ci-bootstrap.sh
+++ ci-bootstrap.sh
@@ -26,7 +26,10 @@ if [ "$(unamer)" = "Darwin" ]; then
   if [ "$(uname -r | cut -f1 -d'.')" = "21" ]; then
     XCODE_VERSION="13.4.1"
   else
-    XCODE_VERSION="15.4.0"
+    # Stick to 15.2 because current default download version of Coverity cannot support higher yet:
+    # https://sig-synopsys.my.site.com/community/s/article/coverity-capture-failed-for-Xcode-15-4-version-on-Mac-OS
+    # https://documentation.blackduck.com/bundle/coverity-docs/page/webhelp-files/relnotes_latest.html#:~:text=Bug%20fixes-,CAP%2D2296,-Reported%20in%20version
+    XCODE_VERSION="15.2.0"
   fi
 
   case "${PROJECT_TYPE}" in 
